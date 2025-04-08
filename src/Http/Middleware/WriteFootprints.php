ff0fe6ce70f562d8a7e83cca179da9bd7f69a11a
Justice Sandram <justicesandram@gmail.com>
2025-04-08 09:18:16 +0200

    feat: add file logging of footprints
    Update composer dependencies to their latest versions.
    
    Add the ability to log footprints to a file via configuration.
    This allows for easier debugging and analysis of user activity.

--- src/Http/Middleware/WriteFootprints.php
+++ src/Http/Middleware/WriteFootprints.php
@@ -4,34 +4,32 @@ namespace TNM\Footprints\Http\Middleware;
 
 use Closure;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Log;
 use TNM\Footprints\Models\Footprint;
 
 class WriteFootprints
 {
     public function handle(Request $request, Closure $next)
     {
-        if (!defined('LARAVEL_START')) define('LARAVEL_START', microtime(true));
+        if (!defined('LARAVEL_START'))
+            define('LARAVEL_START', microtime(true));
 
         return $next($request);
     }
 
-    public function terminate(Request $request, $response): void
+    public function terminate(Request $request, Response $response): void
     {
-        Footprint::create([
-            'endpoint' => $request->route() ? $request->route()->uri : '',
-            'uri' => $request->getRequestUri(),
-            'user_type' => $request->user() ? get_class($request->user()) : null,
-            'user_id' => auth()?->id(),
-            'method' => $request->method(),
-            'ip_address' => $request->ip(),
-            'request' => json_encode($this->maskHiddenFields($request->all())),
-            'request_headers' => json_encode($request->headers->all()),
-            'content' => empty($request->all()) ? $request->getContent() : null,
-            'response' => method_exists($response, 'content') ? $response->content() : null,
-            'milliseconds' => $this->getTurnAroundTime(),
-            'status' => method_exists($response, 'status') ? $response->status() : null,
-            'success' => $response->isSuccessful(),
-        ]);
+        if (config('footprints.log_footprints_to_file') === false) {
+            Footprint::create($this->getFootprint($request, $response));
+            return;
+        }
+
+        Log::channel('stack')->info(
+            'Footprint Data',
+            $this->getFootprint($request, $response)
+        );
+
     }
 
     private function maskHiddenFields(array $request): array
@@ -39,7 +37,7 @@ class WriteFootprints
         $hiddenFields = $this->getHiddenFields();
         $mask = [];
         foreach ($request as $key => $value) {
-            if (in_array($key, $hiddenFields,)) {
+            if (in_array($key, $hiddenFields, )) {
                 $value = '{{secret}}';
             }
             $mask[$key] = $value;
@@ -55,5 +53,24 @@ class WriteFootprints
     private function getTurnAroundTime(): float|int
     {
         return round(round(microtime(true) - LARAVEL_START, 4) * 1000);
+    }
+
+    private function getFootprint(Request $request, Response $response): array
+    {
+        return [
+            'endpoint' => $request->route() ? $request->route()->uri : '',
+            'uri' => $request->getRequestUri(),
+            'user_type' => $request->user() ? get_class($request->user()) : null,
+            'user_id' => auth()?->id(),
+            'method' => $request->method(),
+            'ip_address' => $request->ip(),
+            'request' => json_encode($this->maskHiddenFields($request->all())),
+            'request_headers' => json_encode($request->headers->all()),
+            'content' => empty($request->all()) ? $request->getContent() : null,
+            'response' => method_exists($response, 'content') ? $response->content() : null,
+            'milliseconds' => $this->getTurnAroundTime(),
+            'status' => method_exists($response, 'status') ? $response->status() : null,
+            'success' => $response->isSuccessful(),
+        ];
     }
 }
--- src/config/config.php
+++ src/config/config.php
@@ -3,4 +3,5 @@
 return [
     'retention_days' => env('FOOTPRINTS_RETENTION_DAYS', 30),
     'hidden_fields' => env('FOOTPRINTS_HIDDEN_FIELDS', 'password,pin,new_pin'),
+    'log_footprints_to_file' => env('LOG_FOOTPRINT_TO_FILE',  true)
 ];
