c2c344d314e355ccf648aa90da8c41a5f7e194e6
Justice Sandram <108130114+justicesandram@users.noreply.github.com>
2025-05-21 12:13:08 +0200

    feat: enable logging to an elasticsearch data stream

--- src/Logging/ElasticsearchHandler.php
+++ src/Logging/ElasticsearchHandler.php
@@ -2,6 +2,8 @@
 namespace TNM\Footprints\Logging;
 
 use Throwable;
+use DateTimeImmutable;
+use DateTimeInterface;
 use Illuminate\Support\Facades\Log;
 use Monolog\Handler\ElasticsearchHandler as BaseHandler;
 
@@ -10,7 +12,18 @@ final class ElasticsearchHandler extends BaseHandler
     protected function bulkSend(array $records): void
     {
         try {
+
+            if (isset($records[0])) {
+                $datetime = $records[0]['datetime'] ?? (new DateTimeImmutable())
+                    ->format(DateTimeInterface::ATOM);
+
+                unset($records[0]['datetime']);
+
+                $records[0]['@timestamp'] = $datetime;
+            }
+
             parent::bulkSend($records);
+
         } catch (Throwable $e) {
             Log::channel('daily')
                 ->warning('Elasticsearch logging failed: ' . $e->getMessage(), [
